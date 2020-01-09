58caf5a49f86f9fde64738a78f6c2b40dbc98643
Andres Rodriguez <andresrodriguez64@yahoo.com>
2020-01-08 16:09:50 -0800

    Added images to slideshow gallery for the second project

--- gallery-control.js
+++ gallery-control.js
@@ -23,3 +23,29 @@ function showDivs(n) {
 	}
 	x[slideIndex - 1].style.display = 'block';
 }
+
+var webSlideIndex = 1;
+showWebDivs(webSlideIndex);
+
+function plusWebDivs(n) {
+	showWebDivs((webSlideIndex += n));
+}
+
+function currentWebDiv(n) {
+	showWebDivs((webSlideIndex = n));
+}
+
+function showWebDivs(n) {
+	let i;
+	let x = document.getElementsByClassName('web-code-slides');
+	if (n > x.length) {
+		webSlideIndex = 1;
+	}
+	if (n < 1) {
+		webSlideIndex = x.length;
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = 'none';
+	}
+	x[webSlideIndex - 1].style.display = 'block';
+}
